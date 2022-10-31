eb9211583f14b46d703c7a8408160ecd3631c7c2
qkransrb <qkransrb90@gmail.com>
2022-10-31 16:41:31 +0900

    initial commit

---
+++ src/test/java/com/example/wdemo/WdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
